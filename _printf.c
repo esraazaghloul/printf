0cddf43dc83c6f252baa012dc0b865fa384b7475
esraazaghloul <esraazaghloul@live.com>
2023-11-21 22:33:29 -0800

    3

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _printf - function that prints
+ * @format: format string
+ *
+ * Return: bytes printed
+*/
+
+int _printf(const char *format, ...)
+{
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
+
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params))
+		{
+			p++;
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
